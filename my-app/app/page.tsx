24fb6888d2e7264ebf31954c35ea3d260efe846a
Muhammad Bilawal Zaman <143614305+mbilawalzaman@users.noreply.github.com>
2024-07-16 17:37:52 +0000

    removed Head warning

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,22 +1,25 @@
-import Head from "next/head";
-import Image from "next/image";
-import Navbar from "./components/navbar";
-import Hero from "./components/hero";
-import Goal from "./components/goal";
-import Services from "./components/services";
-import Team from "./components/team";
-import Testimonials from "./components/testimonials";
-import Contact from "./components/contact";
-import Footer from "./components/footer";
- 
+import Head from 'next/head';
+import { Metadata } from 'next';
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Goal from './components/goal';
+import Services from './components/services';
+import Team from './components/team';
+import Testimonials from './components/testimonials';
+import Contact from './components/contact';
+import Footer from './components/footer';
+
 const Home = () => {
   return (
     <div className="flex min-h-screen flex-col py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <Metadata>
+          <title>My Page Title</title>
+          <link rel="icon" href="/favicon.ico" />
+          {/* Add any additional metadata tags here */}
+        </Metadata>
       </Head>
- 
+
       <Navbar />
       <main className="relative flex-1 lg:container text-center p-0 mx-auto overflow-x-hidden overflow-auto">
         <Hero />
@@ -24,11 +27,11 @@ const Home = () => {
         <Services />
         <Testimonials />
         <Team />
-        <Contact/>
+        <Contact />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
- 
+
 export default Home;
